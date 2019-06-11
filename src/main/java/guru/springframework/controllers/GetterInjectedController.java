e317a2065920c45e913d3188c6f6c9d7a7084488
Clement TRIBOUILLARD <clement.tribouillard@siicanada.com>
2019-06-11 11:48:15 -0400

    autowired annotation

--- src/main/java/guru/springframework/controllers/GetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/GetterInjectedController.java
@@ -1,15 +1,20 @@
 package guru.springframework.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.services.GreetingService;
 
+@Controller
 public class GetterInjectedController {
 	
 	private GreetingService greetingService;
 	
-	String sayHello() {
+	public String sayHello() {
 		return greetingService.sayGreeting();
 	}
 	
+	@Autowired
 	public void setGreetingService(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
