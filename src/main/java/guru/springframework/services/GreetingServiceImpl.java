8b2a889497203e0e6a3c65c1dd48d8032d40a3ad
Clement TRIBOUILLARD <clement.tribouillard@siicanada.com>
2019-06-11 14:30:55 -0400

    primary annotation

--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 	
-	public static final String HELLO_GURUS = "hello clemtrib!";
+	public static final String HELLO_GURUS = "hello clemtrib! - Original";
 	
 	@Override
 	public String sayGreeting() {
